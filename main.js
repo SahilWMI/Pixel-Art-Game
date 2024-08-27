ab643d608a2ed3fef69366fb29cc716457ed1d1d
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-08-27 23:26:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
+const TILE_SIZE = 32;
 const COLS = 15;
 const ROWS = 20;
 const GAME_WIDTH = TILE_SIZE * COLS;
 const GAME_HEIGHT = TILE_SIZE * ROWS;
 
-window.addEventListener('load', function()){
+window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = GAME_WIDTH;
     canvas.height = GAME_HEIGHT;
-}
+})
