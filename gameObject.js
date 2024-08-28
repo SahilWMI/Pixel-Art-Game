cc3cc891c78aaea5b6957e95a119de6e3fc4507e
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-08-28 23:23:58 +0530

    Update gameObject.js

--- gameObject.js
+++ gameObject.js
@@ -1,6 +1,15 @@
+import {TILE_SIZE} from "../main.js"
+
 export class GameObject {
     constructor(){
 
     }
-    
+    draw(ctx){
+        ctx.fillRect(
+            0,
+            0,
+            TILE_SIZE,
+            TILE_SIZE
+        )
+    }
 }
