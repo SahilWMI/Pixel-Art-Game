753b8d829c62fd657868b4c6c023f9a986f601e4
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-08-28 23:16:37 +0530

    Update world.js

--- world.js
+++ world.js
@@ -4,4 +4,16 @@ export class World {
     constructor(){
         this.level1 = {}
     }
+    drawGrid(ctx){
+        for (let row = 0; row < ROWS; row++){
+            for (let col = 0; col < COLS; col++){
+                ctx.strokeRect(
+                    col * TILE_SIZE,
+                    row * TILE_SIZE,
+                    TILE_SIZE,
+                    TILE_SIZE
+                )
+            }
+        }
+    }
 }
